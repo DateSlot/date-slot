3320d90683a26e38bf12e46aa375c8ae6a31d429
Vincent Mathis <vincentmathis@protonmail.com>
2026-06-27 14:16:58 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.post("/api/rsvp", (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(resolve(__dirname, "..", "dist")));
-  app.get("/{*path}", (_req, res) => {
+  app.use((_req, res) => {
     res.sendFile(resolve(__dirname, "..", "dist", "index.html"));
   });
 }
